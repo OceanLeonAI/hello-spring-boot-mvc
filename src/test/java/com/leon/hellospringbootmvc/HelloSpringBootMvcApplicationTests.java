a61dd408fcb61afcf49310effae169eb01d5bf02
OceanLeonAI <beyondjayfir@126.com>
2022-02-21 17:04:50 +0800

    init repository

---
+++ src/test/java/com/leon/hellospringbootmvc/HelloSpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.hellospringbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
